f73d6e9eab08939604d314f5f4b1249cede57f19
martin444 <alejandrofarel62@gmail.com>
2022-08-25 16:23:18 -0300

    change env

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,11 +2,6 @@ import { Global, Module, UnauthorizedException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import config from '../config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
-import { Address } from 'src/user/entities/address.entity';
-import { Documents } from 'src/user/entities/document.entity';
-import { PersonalInfo } from 'src/user/entities/personal_info.entity';
-import { Wallet } from 'src/wallet/entitys/wallet.entity';
 
 @Global()
 @Module({
@@ -24,7 +19,6 @@ import { Wallet } from 'src/wallet/entitys/wallet.entity';
                         password: configService.mysql.password,
                         user: configService.mysql.user,
                         host: configService.mysql.host,
-                        entities: [User, Address, Documents, PersonalInfo, Wallet],
                         synchronize: true,
                         autoLoadEntities: true,
                         ssl: false,
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -15,10 +15,10 @@ import { Storages3Service } from 'src/storages3/storages3.service';
 @Global()
 @Module({
     imports: [TypeOrmModule.forFeature([
-        User,
         Address,
         Documents,
         PersonalInfo,
+        User,
     ], 'mysqlDB')],
     controllers: [UserController, ClientsController],
     providers: [UserService, AddressService, DocumentsService, Storages3Service],
